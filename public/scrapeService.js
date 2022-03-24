8678b0c8761e24308b703ee102f084cda2a74380
Alexander Nylund <55355086+slightly-blue@users.noreply.github.com>
2022-03-24 22:10:57 +0200

    meta regex and ld+json parsing
    Now works on most Finnish news sites

--- public/scrapeService.js
+++ public/scrapeService.js
@@ -4,125 +4,258 @@
 const scrapeService = async (test_url) => {
   const axios = require('axios');
   const cheerio = require('cheerio');
+  // time validation
+  const ISO_8601 = /^\d{4}(-\d\d(-\d\d(T\d\d:\d\d(:\d\d)?(\.\d+)?(([+-]\d\d:\d\d)|Z)?)?)?)?$/i
+
   const document_info = {
-    title: "",
-    type: "",
-    author: "",
-    authors: [
-      // {
-      //   author: "",
-      //   institution: "",
-      // }
-    ],
-    published_date: "",
-    journal: "",
-    publisher: "",
-    volume_no: "",
-    issue_no: "",
-    pages_used: "",
-    doi: "",
-    database: "",
+    title: undefined,
+    type: undefined,
+    authors: undefined,
+    published_date: undefined,
+    journal: undefined,
+    publisher: undefined,
+    volume_no: undefined,
+    issue_no: undefined,
+    pages_used: undefined,
+    doi: undefined,
+    database: undefined,
     URL: test_url,
-    access_date: "",
-    html: '',
+    access_date: undefined,
+    html: undefined,
   }
 
 
   return axios.get(test_url)
     .then(res => {
-
       //console.log(res.data)
       //document_info.html = res
       const $ = cheerio.load(res.data)
 
-      // Authors 
+      const metaNameCheck = (names) => {
+        // Checks trough meta names for first valid name(s) and returns content
+        for (let i = 0; i < names.length; i++) {
+          if ($('meta[name=' + names[i] + ']').length > 1) {
+            let arr = []
+            $('meta[name=' + names[i] + ']').each((element) => {
+              arr.push($(element).attr('content'))
+            });
+            return arr
 
-      if ($('meta[name=citation_author]').length){
-        $('meta[name=citation_author]').each((index, element) => {
-          document_info.authors.push(
-            {
-              author: $(element).attr('content'),
-              institution: $('meta[name=citation_author_institution]').eq(index).attr('content') ?? ""
+          } else {
+            let content = $('meta[name=' + names[i] + ']').attr('content')
+            if (content) {
+              return content
             }
-          )
-        });
-      } else {
-        $('meta[name=dc.creator]').each((index, element) => {
-          document_info.authors.push(
-            {
-              author: $(element).attr('content'),
-              institution: ""
+          }
+        }
+        return undefined
+      }
+      const metaPropertyCheck = (names) => {
+        // Checks trough meta names for first valid name(s) and returns content
+        for (let i = 0; i < names.length; i++) {
+          if ($('meta[property=' + names[i] + ']').length > 1) {
+            let arr = []
+            $('meta[property=' + names[i] + ']').each((element) => {
+              arr.push($(element).attr('content'))
+            });
+            return arr
+
+          } else {
+            let content = $('meta[property=' + names[i] + ']').attr('content')
+            if (content) {
+              return content
             }
-          )
-        });
+          }
+        }
+        return undefined
+      }
+      const itemPropCheck = (names) => {
+        for (let i = 0; i < names.length; i++) {
+          if ($('[itemprop="' + names[i] + '"]').length > 1) {
+            let arr = []
+            $('[itemprop="' + names[i] + '"]').each((element) => {
+              arr.push($(element).attr('content') ?? $(element).text())
+            });
+            return arr
+
+          } else {
+            let content = $('[itemprop="' + names[i] + '"]').attr('content') ?? $('[itemprop="' + names[i] + '"]').text().replace(/^\s+|\s+$/g, '');
+            if (content) {
+              return content
+            }
+          }
+        }
+        return undefined
       }
 
-      // Title 
+      // Get info from application/ld+json 
+      // docs: https://json-ld.org/
+      // docs: https://developers.google.com/search/docs/advanced/structured-data/intro-structured-data
+      const ld_json = () => {
+        try {
+          const ld = JSON.parse($('script[type="application/ld+json"]').html())
+          if (ld) { return ld["@graph"][0] }
+        } catch (e) { console.log(e) }
+      }
 
-      document_info.title = $('meta[name=citation_title]').attr('content') 
-                        ??  $('meta[name=dc.title]').attr('content')
+      const metaNameRegexCheck = (regex) => {
+        const tags = 
+          $('meta').filter(function () {  // match any meta name tag with publish in it with a ISO_8601 date string 
+            return (regex).test($(this).attr('name'))
+          })
+        if(tags.length > 1) { // multiple has not been verified to work
+          let arr = []
+          tags.each((el) => {
+            arr.push($(el).attr('content'))
+          })
+          return arr
+        } else {
+          return tags.attr('content') // works
+        }
+      }
 
-      // Date
+      // get as much info as possible from meta tags 
+      document_info.authors =
+        metaNameCheck([
+          'citation_author',
+          'citation_authors',
+          'dcterms.creator',
+          'dc.creator',
+          'eprints.creators_name',
+          'bepress_citation_author'
+        ]) ??
+        itemPropCheck(['author']) ??
+        ld_json()?.author.map(author => { return author.name }) ??
+        metaNameRegexCheck(/author/i)
 
-      document_info.published_date = $('meta[name=prism.publicationDate]').attr('content') 
-                                  ??  $('meta[name=dc.date]').attr('content')
+        // todo data-rh ?
 
-      // journal 
-      document_info.journal = $('meta[name=citation_journal_title]').attr('content') 
-                          ??  $('meta[name=prism.publicationName]').attr('content')
-                          
-      // publisher 
-      document_info.publisher = $('meta[name=citation_publisher]').attr('content') 
-      ??  $('meta[name=dc.publisher]').attr('content')
+      document_info.authors_institution =
+        metaNameCheck([
+          'citation_author_institution',
+          'prism.place',
+          'bepress_citation_author_institution',
+          'pur.place'
+        ])
 
+      document_info.title =
+        metaNameCheck([
+          'citation_title',
+          'dc.title',
+          'dcterms.title',
+          'eprints.title',
+          'bepress_citation_title',
+          'prism.alternateTitle'
+        ]) ??
+        itemPropCheck(['headline']) ??
+        $('title').text()
 
-      // volume
-      document_info.volume_no = $('meta[name=citation_volume]').attr('content') 
-      ??  $('meta[name=prism.volume]').attr('content')
+      document_info.published_date =
+        metaNameCheck([
+          'citation_date',
+          'prism.publicationDate',
+          'dc.date',
+          'dcterms.date',
+          'dcterms.created',
+          'eprints.date',
+          'bepress_citation_date',
+          'eprints.datestamp'
+        ]) ??
+        itemPropCheck(['datePublished']) ??
+        metaNameCheck(['article:published_time']) ??
+        metaPropertyCheck(['article:published_time']) ??
+        ld_json()?.datePublished ??
 
-      // issue 
-      document_info.issue_no = $('meta[name=citation_issue]').attr('content') 
+        $('meta').filter(function () {  // match any meta name tag with publish in it with a ISO_8601 date string 
+          return (/publish/i).test($(this).attr('name')) && ISO_8601.test($(this).attr('content'))
+        }).attr('content')
 
-      //document_info.pages_used = $('meta[name=citation_volume]').attr('content') 
+      //document_info.published_date = itemPropCheck(['datePublished'])
 
-      document_info.doi = $('meta[name=DOI]').attr('content') ??  $('meta[name=prism.doi]').attr('content')
+      document_info.type_of_work =
+        metaNameCheck([
+          'citation_dissertation_name',
+          'dcterms.type',
+          'dc.type',
+          'prism.contentType',
+          'eprints.type'
+        ])
 
-      document_info.type = $('meta[name=citation_article_type]').attr('content') ?? $('meta[name=dc.type]').attr('content') 
+      document_info.journal =
+        metaNameCheck([
+          'citation_journal_title',
+          'prism.publicationName'
+        ])
 
-      //document_info.URL = $('meta[name=citation_volume]').attr('content') 
+      document_info.publisher =
+        metaNameCheck([
+          'citation_publisher',
+          'dc.publisher',
+          'dcterms.publisher',
+          'prism.corporateEntity',
+          'eprints.publisher',
+          'bepress_citation_publisher'
+        ]) ??
+        ld_json()?.publisher?.name
+
+      document_info.volume_no =
+        metaNameCheck([
+          'citation_volume',
+          'prism.volume',
+          'pur.volume',
+          'eprints.volume',
+          'bepress_citation_volume'
+        ])
+
+      document_info.issue_no =
+        metaNameCheck([
+          'citation_issue',
+          'prism.number',
+          'pur.number',
+          'eprints.number',
+          'bepress_citation_issue'
+        ])
+
+      document_info.doi =
+        metaNameCheck([
+          'DOI',
+          'prism.doi',
+          'bepress_citation_doi',
+          'dcterms.identifier',
+          'dc.identifier',
+          'eprints.id_number'
+        ])
 
       const today = new Date();
-      document_info.access_date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
- 
-      //document_info.html = $('meta[name=citation_volume]').attr('content') 
+      document_info.access_date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
 
-      // dc.
-      // prism.
-      // citation_
+      //console.log(document_info) 
 
-      console.log(document_info)
-      // 
+
       return document_info
 
     }).catch(err => {
-      console.error(err) 
-      throw err
+      console.error(err)
+      return err
     })
 
 
   // inspiration: http://div.div1.com.au/div-thoughts/div-commentaries/66-div-commentary-metadata
-  // 1. check type of source
+  // 1. Check link type
+  // 2. Check whois database 
 
-  // 2. find author
 
-  // 3. find title of article or work
-  // <title> tag
-
-  // 4. find published date 
+  // Strategies:
+  // - file endings
+  // - meta tags
+  // - DOI API lookup if available 
+  // - pdf metadata 
+  // - HTML itemprop attribute  
+  // - 
 
   // use axios 
 
- 
+
 }
 module.exports = { scrapeService }
 //export default scrapeService
--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-import CreateReference from './components/create_reference';
+import './App.css'
+import CreateReference from './components/create_reference'
 
 function App() {
   return (
     <div className="App">
       <CreateReference />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
